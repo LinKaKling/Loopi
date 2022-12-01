9f08fa1b9835e8b7f0442be12b75a471fac6b641
JonasSuchy <Jonas.Suchy@gmail.com>
2022-12-01 08:44:10 +0100

    In ViewModel verschoben.

--- LoopiBusines/SoundTest.cs
+++ LoopiBusines/SoundTest.cs
@@ -6,57 +6,57 @@ namespace LoopiBusines
 {
     public class play
     {
-        public WaveIn waveSource = null;
-        public WaveFileWriter waveFile = null;
+        //public WaveIn waveSource = null;
+        //public WaveFileWriter waveFile = null;
 
-        private void StartBtn_Click(object sender, EventArgs e)
-        {
-            StartBtn.Enabled = false;
-            StopBtn.Enabled = true;
+        //private void StartBtn_Click(object sender, EventArgs e)
+        //{
+        //    StartBtn.Enabled = false;
+        //    StopBtn.Enabled = true;
 
-            waveSource = new WaveIn();
-            waveSource.WaveFormat = new WaveFormat(44100, 1);
+        //    waveSource = new WaveIn();
+        //    waveSource.WaveFormat = new WaveFormat(44100, 1);
 
-            waveSource.DataAvailable += new EventHandler<WaveInEventArgs>(waveSource_DataAvailable);
-            waveSource.RecordingStopped += new EventHandler<StoppedEventArgs>(waveSource_RecordingStopped);
+        //    waveSource.DataAvailable += new EventHandler<WaveInEventArgs>(waveSource_DataAvailable);
+        //    waveSource.RecordingStopped += new EventHandler<StoppedEventArgs>(waveSource_RecordingStopped);
 
-            waveFile = new WaveFileWriter(@"C:\Temp\Test0001.wav", waveSource.WaveFormat);
+        //    waveFile = new WaveFileWriter(@"C:\Temp\Test0001.wav", waveSource.WaveFormat);
 
-            waveSource.StartRecording();
-        }
+        //    waveSource.StartRecording();
+        //}
 
-        private void StopBtn_Click(object sender, EventArgs e)
-        {
-            StopBtn.Enabled = false;
+        //private void StopBtn_Click(object sender, EventArgs e)
+        //{
+        //    StopBtn.Enabled = false;
 
-            waveSource.StopRecording();
-        }
+        //    waveSource.StopRecording();
+        //}
 
-        void waveSource_DataAvailable(object sender, WaveInEventArgs e)
-        {
-            if (waveFile != null)
-            {
-                waveFile.Write(e.Buffer, 0, e.BytesRecorded);
-                waveFile.Flush();
-            }
-        }
+        //void waveSource_DataAvailable(object sender, WaveInEventArgs e)
+        //{
+        //    if (waveFile != null)
+        //    {
+        //        waveFile.Write(e.Buffer, 0, e.BytesRecorded);
+        //        waveFile.Flush();
+        //    }
+        //}
 
-        void waveSource_RecordingStopped(object sender, StoppedEventArgs e)
-        {
-            if (waveSource != null)
-            {
-                waveSource.Dispose();
-                waveSource = null;
-            }
+        //void waveSource_RecordingStopped(object sender, StoppedEventArgs e)
+        //{
+        //    if (waveSource != null)
+        //    {
+        //        waveSource.Dispose();
+        //        waveSource = null;
+        //    }
 
-            if (waveFile != null)
-            {
-                waveFile.Dispose();
-                waveFile = null;
-            }
+        //    if (waveFile != null)
+        //    {
+        //        waveFile.Dispose();
+        //        waveFile = null;
+        //    }
 
-            StartBtn.Enabled = true;
-        }
+        //    StartBtn.Enabled = true;
+        //}
     }
 
 }
--- Loopitest/ViewModels/MainWindowViewModel.cs
+++ Loopitest/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,66 @@
 ﻿
+using Microsoft.VisualBasic;
+using NAudio.Wave;
+using ReactiveUI;
+using System;
+using System.Windows.Input;
+
 namespace Loopitest.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        private IWaveIn waveSource = null;
+        private WaveFileWriter waveFile = null;
+        public ICommand StartCommand { get; }
+        public ICommand StopCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            StartCommand = ReactiveCommand.Create(Start);
+        }
+
+        private void Start()
+        {
+            waveSource = new WaveIn();
+            waveSource.WaveFormat = new WaveFormat(44100, 1);
+
+            waveSource.DataAvailable += new EventHandler<WaveInEventArgs>(waveSource_DataAvailable);
+            waveSource.RecordingStopped += new EventHandler<StoppedEventArgs>(waveSource_RecordingStopped);
+
+            waveFile = new WaveFileWriter(@"C:\Temp\Test0001.wav", waveSource.WaveFormat);
+
+            waveSource.StartRecording();
+        }
+
+        private void Stop(object sender, EventArgs e)
+        {
+            waveSource.StopRecording();
+        }
+
+        void waveSource_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (waveFile != null)
+            {
+                waveFile.Write(e.Buffer, 0, e.BytesRecorded);
+                waveFile.Flush();
+            }
+        }
+
+        void waveSource_RecordingStopped(object sender, StoppedEventArgs e)
+        {
+            if (waveSource != null)
+            {
+                waveSource.Dispose();
+                waveSource = null;
+            }
+
+            if (waveFile != null)
+            {
+                waveFile.Dispose();
+                waveFile = null;
+            }
+
+            StartBtn.Enabled = true;
+        }
     }
 }
